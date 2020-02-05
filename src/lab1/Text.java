8a888781c2845d773167431f3c8214c83b7110f4
annastasiat <anastasia.taftay@gmail.com>
2020-02-05 21:32:36 +0200

    more minor changes

--- src/lab1/Text.java
+++ src/lab1/Text.java
@@ -1,10 +1,9 @@
 package lab1;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.*;
 
 public class Text {
@@ -12,14 +11,14 @@ public class Text {
     private String filename;
     private boolean spaces;
 
-    public Text(String filename, Boolean spaces) {
+    public Text(String filename, boolean spaces) {
         Path path = Paths.get("texts/" + filename);
         this.filename = filename;
         this.spaces = spaces;
         try {
             str = String.join(" ", Files.readAllLines(path))
                     .toLowerCase()
-                    .replaceAll(spaces?"[^а-я ё]":"[^а-яё]", "")
+                    .replaceAll(spaces ? "[^а-я ё]" : "[^а-яё]", "")
                     .replaceAll("[ ]{2,}", " ")
                     .trim();
         } catch (IOException e) {
@@ -29,6 +28,8 @@ public class Text {
 
     private Map<String, Double> ngrams(int n) {
         Map<String, Double> ngramsProbs = new HashMap<>();
+        //Pr(ngram) = amount of ngram / amount of all ngrams = amount of ngram *(1 / amount of all ngrams) =
+        // = amount of ngram *(1 / (str.length() - n + 1))
         double probabilityBit = 1.0 / (str.length() - n + 1);
 
         for (int i = 0; i <= str.length() - n; i++) {
@@ -42,31 +43,29 @@ public class Text {
         Map<String, Double> ngramsProbs = ngrams(n);
         double h = 0;
         for (double i : ngramsProbs.values()) {
-            h += i * Math.log(i); // / Math.log(2);
+            h += i * Math.log(i);
         }
-        return -h / (Math.log(2) * n);
+        return -h / (Math.log(2) * n); //Math.log - natural logarithm, so log2(i) = log(i)/log(2)
     }
+
 
     public void printNgrams(int n) {
         List<Map.Entry<String, Double>> ngramsProbs = new ArrayList<>(ngrams(n).entrySet());
-        StringBuilder resStr = new StringBuilder();
-        //sort:
+        String outFilename = "results/" + n + "_grams_" + (spaces ? "" : "no_spaces_")
+                + filename.replaceAll("\\.txt", "") + ".tsv";
+
+        //sort
         ngramsProbs.sort(Comparator.comparing(Map.Entry::getValue, Comparator.reverseOrder()));
-        //build string for file:
-        for (Map.Entry<String, Double> i : ngramsProbs) {
-            resStr.append(i.getKey()).append("\t").append(i.getValue()).append("\n");
-        }
-        //print to file:
-        try {
-            Path path = Paths.get("results/" + n + "_grams_"
-                    + (spaces ? "" : "no_spaces_")+ filename.replaceAll("\\.txt", "") + ".tsv");
-            Files.write(path, (n + "грамма\tчастота\n").getBytes());
-            Files.write(path, resStr.toString().getBytes(), StandardOpenOption.APPEND);
+
+        try (OutputStream out = new FileOutputStream(outFilename)) {
+            out.write((n + "грамма\tчастота\n").getBytes());
+            for (Map.Entry<String, Double> i : ngramsProbs) {
+                out.write((i.getKey() + "\t" + i.getValue() + "\n").getBytes());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 
     public int getLength() {
         return str.length();
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,13 +5,21 @@ import lab1.Text;
 public class Main {
 
     public static void main(String[] args) {
-        for(String filename: new String[]{"Отверженные. Виктор Гюго.txt", "Улисс.txt"}){
+        double H0 = Math.log(34)/Math.log(2);
+        System.out.println("H0: " + H0);
+
+        /*for(String filename: new String[]{"Отверженные. Виктор Гюго.txt", "Улисс.txt", "voyna-i-mir-tom-1.txt"}){
             Text text = new Text(filename, true);
+            double H;
             System.out.println("\n" + filename);
             System.out.println("with spaces:" );
             System.out.println("Text length(symbols): " + text.getLength());
-            System.out.println("H1: " + text.H(1));
-            System.out.println("H2: " + text.H(2) + "\n");
+            H = text.H(1);
+            System.out.println("H1: " + H);
+            System.out.println("Redundancy(H1): " + (1-H/H0));
+            H = text.H(2);
+            System.out.println("H2: " + H);
+            System.out.println("Redundancy(H2): " + (1-H/H0) + "\n");
             //text.printNgrams(1);
             //text.printNgrams(2);
 
@@ -19,21 +27,24 @@ public class Main {
             System.out.println(filename);
             System.out.println("with no spaces:" );
             System.out.println("Text length(symbols): " + text.getLength());
-            System.out.println("H1: " + text.H(1));
-            System.out.println("H2: " + text.H(2));
+            H = text.H(1);
+            System.out.println("H1: " + H);
+            System.out.println("Redundancy(H1): " + (1-H/H0));
+            H = text.H(2);
+            System.out.println("H2: " + H);
+            System.out.println("Redundancy(H2): " + (1-H/H0) + "\n");
             //text.printNgrams(1);
             //text.printNgrams(2);
-        }
+        }*/
 
-        /*String filename = "Отверженные. Виктор Гюго.txt";//"Улисс.txt";//"voyna-i-mir-tom-1.txt";
-        Text text = new Text(filename);
+        String filename = "voyna-i-mir-tom-1.txt";
+        Text text = new Text(filename, false);
 
 
         System.out.println("\nText length(symbols): " + text.getLength());
         System.out.println("H1: " + text.H(1));
         System.out.println("H2: " + text.H(2));
 
-        text.printNgrams(1);
-        text.printNgrams(2);*/
+        text.printNgrams(2);
     }
 }
